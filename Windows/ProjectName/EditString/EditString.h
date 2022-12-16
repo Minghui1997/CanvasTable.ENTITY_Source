14e70fd18901bada5c18edd49b2a44772919a48b
MingHui <103197921+MingHui1997@users.noreply.github.com>
2022-12-16 18:14:50 +0800

    Add files via upload

---
+++ Windows/ProjectName/EditString/EditString.h
@@ -0,0 +1,7 @@
+﻿void strslice(char *dest,char *sour,int s,int e);
+void strclear(char *dest);
+int charindex(char *dest,char fc);
+int charindex_last(char *dest,char fc);
+int strindex(char *dest,char *str);
+int strindex_last(char *dest,char *str);
+void strreplace(char *dest,char *repstr,char *sour);
